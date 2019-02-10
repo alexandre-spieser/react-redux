dbb4b5df30f8b2c5e6c52b136d01c1d65fa77e2d
Alexandre SPIESER <a.spieser@gmail.com>
2019-02-10 20:49:42 +0000

    can call method within jsx

--- src/index.js
+++ src/index.js
@@ -1,8 +1,22 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+function getButtontext() {
+  return "Click on me!";
+}
+
 const App = function() {
-  return <div> Hi there! </div>;
+  return (
+    <div>
+      <label className="label" for="name">
+        Enter name:
+      </label>
+      <input id="name" type="text" />
+      <button style={{ backgroundColor: "blue", color: "white" }}>
+        {getButtontext()}
+      </button>
+    </div>
+  );
 };
 
 ReactDOM.render(<App />, document.querySelector("#root"));
