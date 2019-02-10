06291649d4549e921291893f03c3479c21dddb26
Alexandre SPIESER <a.spieser@gmail.com>
2019-02-10 20:18:04 +0000

    prettier prettifying things on save

--- src/index.js
+++ src/index.js
@@ -1,11 +1,8 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
 
-const App = function(){
-    return <div>  Hi there! </div>
+const App = function() {
+  return <div> Hi there! </div>;
 };
 
-ReactDOM.render(
-    <App />, 
-    document.querySelector("#root")
-);
+ReactDOM.render(<App />, document.querySelector("#root"));
